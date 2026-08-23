67e5ca2164bf84dccb7098301bf233934df40d79
Rishav Kumar Mishra <rishavkumarmishra001@gmail.com>
2026-08-23 23:39:06 +0530

    Time: 3 ms (66.67%), Space: 14 MB (33.57%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int half = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < half; i++) {
+            if (num[i] == '?')
+                leftQ++;
+            else
+                leftSum += num[i] - '0';
+        }
+
+        for (int i = half; i < n; i++) {
+            if (num[i] == '?')
+                rightQ++;
+            else
+                rightSum += num[i] - '0';
+        }
+
+        // Odd number of '?' -> Alice always wins
+        if ((leftQ + rightQ) % 2 == 1)
+            return true;
+
+        // Bob can win only if this exact balance is possible
+        return leftSum - rightSum != 9 * (rightQ - leftQ) / 2;
+    }
+};
